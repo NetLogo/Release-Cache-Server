b9fa72550224b17a513bdfd5415b85850f48f85e
ERSUCC <isaacbrown5763@gmail.com>
2026-07-09 20:14:35 -0500

    Infrastructure: General code and API improvements

--- server.ts
+++ server.ts
@@ -3,71 +3,115 @@ import fs from "fs";
 import path from "path";
 import AdmZip from "adm-zip";
 
+const host = "127.0.0.1";
+const port = 5000;
+
 const server = express();
+
+function sanitizeInfo(info: any, requireVersion: boolean): boolean {
+  if (!info["os"] || !info["arch"] || (requireVersion && !info["version"])) {
+    return false;
+  }
+
+  if (!`${info["os"]}${info["arch"]}`.match(/^\w+$/)) {
+    return false;
+  }
+
+  return !requireVersion || info["version"].match(/^\d\.\d\.\d(-beta\d)?$/);
+}
 
 server.use(express.json({ limit: "64mb" }));
 
 server.post("/get_available_versions", (request, response) => {
-    const info = request.body;
+  const info = request.body;
 
-    if (!`${info["os"]}${info["arch"]}`.match(/^\w+$/)) {
-        response.statusCode = 403;
-        response.end();
+  if (!sanitizeInfo(info, false)) {
+    response.status(400).send();
 
-        return;
+    return;
+  }
+
+  const root = `versions/${info["os"]}/${info["arch"]}`;
+
+  const versions = Object.fromEntries(fs.readdirSync(root, { withFileTypes: true }).filter(file => file.isDirectory()).map(file => {
+    const contents = fs.readFileSync(path.join(file.parentPath, file.name, ".checksum"), "utf-8");
+
+    return [ file.name, contents.trim() ] as [ string, string ];
+  }));
+
+  response.status(200).json(versions);
+});
+
+server.post("/get_version", (request, response) => {
+  const info = request.body;
+
+  if (!sanitizeInfo(info, true)) {
+    response.status(400).send();
+
+    return;
+  }
+
+  const root = `versions/${info["os"]}/${info["arch"]}/${info["version"]}`;
+
+  if (!fs.existsSync(root)) {
+    response.status(400).send();
+
+    return;
+  }
+
+  const zip = new AdmZip();
+
+  fs.readdirSync(root, { withFileTypes: true, recursive: true }).forEach(file => {
+    const full: string = path.join(file.parentPath, file.name);
+    const relative: string = path.relative(root, full);
+
+    if (file.name == ".checksum" || !file.name.endsWith(".checksum")) {
+      zip.addLocalFile(full, "", relative);
     }
+  });
 
-    const root = `versions/${info["os"]}/${info["arch"]}`;
-
-    const versions = Object.fromEntries(fs.readdirSync(root, { withFileTypes: true }).filter(file => file.isDirectory()).map(file => {
-        const contents = fs.readFileSync(path.join(file.parentPath, file.name, ".checksum"), "utf-8");
-
-        return [ file.name, contents.trim() ] as [ string, string ];
-    }));
-
-    response.statusCode = 200;
-    response.contentType("application/json");
-    response.json(versions);
+  response.status(200).contentType("application/zip").send(zip.toBuffer());
 });
 
 server.post("/get_updated_files", (request, response) => {
-    const info = request.body;
+  const info = request.body;
 
-    if (!`${info["os"]}${info["arch"]}`.match(/^\w+$/) || !info["version"].match(/^\d\.\d\.\d(-beta\d)?$/)) {
-        response.statusCode = 403;
-        response.end();
+  if (!sanitizeInfo(info, true)) {
+    response.status(400).send();
 
-        return;
+    return;
+  }
+
+  const root = `versions/${info["os"]}/${info["arch"]}/${info["version"]}`;
+
+  if (!fs.existsSync(root)) {
+    response.status(400).send();
+
+    return;
+  }
+
+  const checksums = info["checksums"];
+
+  const zip = new AdmZip();
+
+  fs.readdirSync(root, { withFileTypes: true, recursive: true }).forEach(file => {
+    const full = path.join(file.parentPath, file.name);
+    const relative = path.relative(root, full);
+
+    if (file.name == ".checksum") {
+      zip.addLocalFile(full, "", relative);
+    } else if (!file.name.endsWith(".checksum")) {
+      const checksum = `${full}.checksum`;
+
+      if (!(relative in checksums) || (fs.existsSync(checksum) && checksums[relative] != fs.readFileSync(checksum, "utf-8").trim())) {
+        zip.addLocalFile(full, "", relative);
+      }
     }
+  });
 
-    const root = `versions/${info["os"]}/${info["arch"]}/${info["version"]}`;
+  response.status(200).contentType("application/zip").send(zip.toBuffer());
+});
 
-    const checksums = info["checksums"];
-
-    const zip = new AdmZip();
-
-    fs.readdirSync(root, { withFileTypes: true, recursive: true }).forEach(file => {
-        const full = path.join(file.parentPath, file.name);
-        const relative = path.relative(root, full);
-
-        if (file.name == ".checksum") {
-            zip.addLocalFile(full, "", relative);
-        }
-
-        else if (!file.name.endsWith(".checksum")) {
-            const checksum = `${full}.checksum`;
-
-            if (!(relative in checksums) || (fs.existsSync(checksum) && checksums[relative] != fs.readFileSync(checksum, "utf-8").trim())) {
-                zip.addLocalFile(full, "", relative);
-            }
-        }
-    });
-
-    response.statusCode = 200;
-    response.contentType("application/zip");
-    response.send(zip.toBuffer());
-})
-
-server.listen(5000, "127.0.0.1");
-
-console.log("Server running at 127.0.0.1:5000");
+server.listen(port, host, () => {
+  console.log(`Server running at ${host}:${port}`);
+});
